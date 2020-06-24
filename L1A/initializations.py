2223cc93a2dce0602057e3cea80dff189fb2ae0c
paselmer <37942073+paselmer@users.noreply.github.com>
2020-06-24 14:08:49 -0400

    initializations.py now reflects v3

--- L1A/initializations.py
+++ L1A/initializations.py
@@ -8,14 +8,13 @@ import datetime as DT
 #       for Unix vs. Windows
 
 # ******** SET THE NAME OF THE PROJECT & FLIGHT DATE ********
-proj_name = 'FIREX'
-flt_date = '08aug19' # in "CPL" form, not "CAMAL" form
-sortie = '19-905'
+proj_name = 'SEAC4RS'
+flt_date = '08aug13' # in "CPL" form, not "CAMAL" form
+sortie = '13-951'
 
 # ******** SET THE TIME RANGE BOUNDARIES FOR DATA PROCESSING ********
-process_start = DT.datetime(2000,9,1,0,0,0) #yr,mon,dy,hr,min,sec
-process_end   = DT.datetime(2020,10,13,0,0,0)
-
+process_start = DT.datetime(2010, 9, 1, 0, 0, 0) # yr,mon,dy,hr,min,sec
+process_end = DT.datetime(2020, 10, 13, 0, 0, 0)
 
 
 # ******** FILE ATTRIBUTES AND PARAMETERS, ANC. FILE NAMES  ********
@@ -34,42 +33,41 @@ est_nav_recs_1file = 5*60*10 + 30
 # Set this to the maximum possible count rate. Don't set > recs in DTT file!
 max_counts = 16000
 # Dead time tables [list]. List in channel order OR ELSE!!!
-DTT_files = ['dttable_355_0135-072102.xdr','dttable_532_11296-021009_30m.xdr',
-    'dttable_1064par_8351-021009_30m.xdr','dttable_1064per_8346-021009_30m.xdr']
+DTT_files = ['dttable_355_0135-072102.xdr', 'dttable_532_11296-021009_30m.xdr',
+             'dttable_1064par_8351-021009_30m.xdr', 'dttable_1064per_8346-021009_30m.xdr']
 # IWG1 records per second. Please make it a float!
 IWG1_hz = 1.0
 # IWG1 data file
-IWG1_file = "IWG1.30Oct2019-2040.txt"
-# nav (refering to file type) records per second. (Please make it a float!) 
+IWG1_file = "IWG1.21Oct2019-2040.txt"
+# nav (referring to file type) records per second. (Please make it a float!)
 nav_hz = 1.0
 # The overlap file to use. Only applies for single overlap case (multi_OLs == False).
-overlap_file = 'olaptable_cpl_D150120.xdr'
-
+overlap_file = 'olaptable_cpl-ccviceax_comb_iceland12.xdr'
 
 
 # ******** PARAMETERS THAT ADJUST DATA, PROCESSING  ********
 # The number of leap seconds since 1980-01-06 00:00:00
 leapsecs = 0
 # Start and end altitudes of solar background region (meters)
-bg_st_alt = -2500.0
-bg_ed_alt = -3800.0
+bg_st_alt = -1500.0
+bg_ed_alt = -3500.0
 # The bin resolution in the fixed frame (m)
 vrZ_ff = 30.0
 # List containing top and bottom altitudes (m) of the fixed frame
-ff_bot_alt,ff_top_alt = [-15e3,22.005e3]
+ff_bot_alt, ff_top_alt = [-12e3, 22.005e3]
 # This flag tells the code which equations to use to convert energy
-e_flg = 12
+e_flg = 6
 # Computed and init. file difference can't exceed this tolerance.
 CLS_hz_tol = 0.05
-# Set the Polarization Gain ratios for the wavelenghts [532nm, 1064nm]
-PGain = [0.00,0.8043]
+# Set the Polarization Gain ratios for the wavelengths [532nm, 1064nm]
+PGain = [0.00, 0.8043]
 # Saturation values, per bin per 500 shots. List in detector order.
 saturation_values = [3080.9, 1570.0, 1420.0, 1470.0]
 # If True, will obtain mult OL info from "overlaps_configuration.csv"
-multi_OLs = True
+multi_OLs = False
 # The number of seconds needed to convert from the instrument's Unix time
 # to UTC. Typically either 5 hours (cold season) or 4 hours (warm season).
-secs_btwn_instr_UnixT_and_UTC = 18000
+secs_btwn_instr_UnixT_and_UTC = 14400
 # Roll and pitch offsets for GPS (degrees). Subtract these from read-in vals.
 gps_roll_offset = 0.0
 gps_pitch_offset = 0.0
@@ -83,13 +81,13 @@ min_avg_profs = 4
 # "Up" or "Down?" Which direction is lidar pointed?
 pointing_dir = "Down"
 # For actual data processing (not the GUI), nav data source
-Nav_source = 'cls' #'nav' 'gps' 'iwg1' or 'cls'
+Nav_source = 'cls' # 'nav' 'gps' 'iwg1' or 'cls'
 # Don't process any data when below this alt (m). Doesn't apply to GUI.
 alt_cutoff = 500
 # Don't process any profiles where off-nadir angle exceeds this many radians.
-ONA_cutoff = 20.0 * (const.pi/180.0)
+ONA_cutoff = 25.0 * (const.pi/180.0)
 # Invalid/bad Nav values get overwritten with this value - datetime.datetime object.
-bad_cls_nav_time_value = DT.datetime(1970,1,1,0,0,0)
+bad_cls_nav_time_value = DT.datetime(1970, 1, 1, 0, 0, 0)
 # The attention bar
 attention_bar = '\n******************************\n'
 # The maximum number of hours a dataset is expected to be
@@ -97,10 +95,15 @@ max_flt_hours = 30
 # The number of standard deviations of EM values to keep. Can set negative to keep all.
 Estds = 4
 # The following 3 parameters correct the reported attitude
-cpl_roll_offset = 0.0#+0.75
-cpl_pitch_offset = 0.0#+0.45
-cpl_pitch_roll_factor = 0.0#-1.0/80.0
-
+cpl_roll_offset = 0.0#0.75
+cpl_pitch_offset = 0.0#0.45
+cpl_pitch_roll_factor = 0.0#-(1.0/80.0)
+# New for cpl_l1a_v3, parameters that control computation of varying data rate
+make_frequency = 1      # set to 1 to do it
+gap = 500               # typically 500, # of recs to integrate
+step_size = 100         # typically 100, m_array updates every this # of recs
+freq_tol = 1e-10        # typically 1e-10, tolerance for freq. calc. loop
+min_jump = 400          # Tested at 400
 
 
 # ******** PARAMETERS THAT DESCRIBE INPUT LIDAR DATA  ********
@@ -112,18 +115,17 @@ nshots = 500
 # CLS data also don't include bin size. So define here (m).
 vrZ = 29.98
 # The laser rep rate in Hertz
-rep_rate = 5142.0
+rep_rate = 5000.0
 # The number of wavelengths
 nwl = 3
 # Which channels #'s are which wavelengths? (0=355,1=532,2=1064)
 wl_map = [0, 1, 2, 2]
 # Create a list where index # of WL corresponds to string name of WL
-wl_str = ['355','532','1064']
+wl_str = ['355', '532', '1064']
 # CLS data records per second. Please make it a float!
 CLS_hz = float(rep_rate) / float(nshots)
 # The resolution of CPL's instrument clock in seconds
 inst_clk_rez = 1.0
-
 
 
 # ******** FOR GUI AND/OR PLOTTING  ********
@@ -137,24 +139,24 @@ CBar_max = 80.0
 CBar_max_NRB = 5e13
 CBar_min = 0.0
 # The order of magnitude of the alt scale (help make round ticks)
-scale_alt_OofM = 1e3 #order of mag.
+scale_alt_OofM = 1e3  # order of mag.
 # Default vertical range of bins to be plotted in curtain
-minbin=833
-maxbin=0
+minbin = 833
+maxbin = 0
 # curtain plot width and height (inches)
-figW = 12
-figL = 8
+figW = 16
+figL = 10
 # profile plot width and height (inches)
 pp_figW = 6.5
 pp_figL = 7
 # default axes limits for profile plot [xmin,xmax]/[ymin,ymax]
-pp_xax_bounds_raw_counts = [0,200]
-pp_xax_bounds_bgsub_counts = [-150,185]
-pp_xax_bounds_NRB = [-7e12,5e13]
-pp_yax_bounds_bins = [833,0]
-pp_yax_bounds_alt = [-10e3,20e3]
+pp_xax_bounds_raw_counts = [0, 100]
+pp_xax_bounds_bgsub_counts = [-15, 45]
+pp_xax_bounds_NRB = [-7e12, 5e13]
+pp_yax_bounds_bins = [833, 0]
+pp_yax_bounds_alt = [-10e3, 20e3]
 # Y-axis bounds of the energy monitor plot
-EMp_yax_bounds = [0,150]
+EMp_yax_bounds = [0, 150]
 # Channel # entries by user cannot be outside this range
 min_chan = 1
 max_chan = 4
@@ -164,8 +166,7 @@ hori_cap = 20000
 CPpad = 0.1
 
 
-
-if (os.name != 'nt'): # IF UNIX-BASED MACHINE, DEFINE DIRECTORIES HERE
+if os.name != 'nt':  # IF UNIX-BASED MACHINE, DEFINE DIRECTORIES HERE
 
     # ******** DEFINE ALL DIRECTORIES ********
     # Set the directory of the raw data
@@ -195,7 +196,6 @@ if (os.name != 'nt'): # IF UNIX-BASED MACHINE, DEFINE DIRECTORIES HERE
     # Directory and name of C++ library with functions to read in DEM
     DEM_lib_path = DEM_dir + 'JPL_DEM_CPP_SO/'
     DEM_Cpp_name = 'JPL_DEM_CPP_FUNCTIONS.so'
-    
 
 else:                 # IF WINDOWS-BASED MACHINE, DEFINE DIRECTORIES HERE
 
