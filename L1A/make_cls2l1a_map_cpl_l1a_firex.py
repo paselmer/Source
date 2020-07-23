significant flaw in averaging section of L1A code fixed [7/23/20]

L1A/make_cls2l1a_map_cpl_l1a_firex.py
@@ -501,19 +501,19 @@ elif Nav_source == 'cls':
 
     # Load the entire nav dataset into memory. This data will be used in both
     # the 'cls' Nav block and later on in the main file loop.
-    cls_meta_data_all, nav2cls_indx, FL_trunc, usable_file_range = read_entire_cls_meta_dataset(raw_dir,file_len_recs,nbins,flt_date,bad_cls_nav_time_value)
-    #meta_save = np.copy(cls_meta_data_all) #uncomment to compare processed to original
+    cls_meta_data_all, nav2cls_indx, FL_trunc, usable_file_range = read_entire_cls_meta_dataset(raw_dir, file_len_recs, nbins, flt_date, bad_cls_nav_time_value)
+    # meta_save = np.copy(cls_meta_data_all) #uncomment to compare processed to original
     cls_nav_data_all = np.copy(cls_meta_data_all['Nav'])
 
-    UnixT_epoch = DT.datetime(1970,1,1)
+    UnixT_epoch = DT.datetime(1970, 1, 1)
     m = 1.0 / CLS_hz
     
     # Identify all the unique UTC_Times and their starting indexes
-    u, ui, ncounts = np.unique(cls_nav_data_all['UTC_Time'],return_index=True,return_counts=True)
+    u, ui, ncounts = np.unique(cls_nav_data_all['UTC_Time'], return_index=True, return_counts=True)
     # Bad time values will sink to the front of this "unique" array
     if u[0] == bad_cls_nav_time_value:
         print(attention_bar)
-        print("\nSome(A) UTC_Time(s) in the Nav records were(was) invalid.")
+        print("\nA(Some) UTC_Time(s) in the Nav record(s) was(were) invalid.")
         print("Attempt is being made to take care of this.") 
         print("Setting first invalid CLS-Nav UTC_Time record equal to")
         print("the first valid CLS-Nav UTC_Time record.\n")
@@ -538,9 +538,9 @@ elif Nav_source == 'cls':
     n_u = u.shape[0]      
     
     # Convert the unique UTC_Times to Unix Times
-    Nav_UnixT = np.zeros(n_u,dtype=np.float64)
-    Nav_unique = np.zeros(n_u,dtype=CLS_decoded_nav_struct)
-    for i in range(0,n_u): 
+    Nav_UnixT = np.zeros(n_u, dtype=np.float64)
+    Nav_unique = np.zeros(n_u, dtype=CLS_decoded_nav_struct)
+    for i in range(0, n_u):
         Nav_UnixT[i] = (cls_nav_data_all['UTC_Time'][ui[i]] - UnixT_epoch).total_seconds()
         Nav_unique[i] = cls_nav_data_all[ui[i]] 
 
@@ -551,20 +551,21 @@ elif Nav_source == 'cls':
         print(attention_bar)
         print("HEY! The data span "+str(tot_hours).strip()+" hours!")
         print("Consider trimming processing time range in init. file.")
-        print('Enter "c" to contine processing anyway.')
+        print('Enter "c" to continue processing anyway.')
         print('Enter "q" to safely quit.')
         print(attention_bar)
         pdb.set_trace() 
 
     # Look at the time deltas between unique, you'll use this in a little bit
-    u, ui, ncounts = np.unique(cls_meta_data_all['Header']['ExactTime'],return_index=True,return_counts=True)
-    if u[0] == bad_cls_nav_time_value:
-        u = u[1:]
-        ui = ui[1:]
-        ncounts = ncounts[1:]    
-    ExactTime_ncounts = np.copy(ncounts)
-    ExactTime_ui = np.copy(ui)
-    inst_clk_deltas = delta_datetime_vector(u)
+    # On 5/4/2020 updated to separate Instrument time from Nav Time u ui and ncounts arrays
+    u_inst, ui_inst, ncounts_inst = np.unique(cls_meta_data_all['Header']['ExactTime'], return_index=True, return_counts=True)
+    if u_inst[0] == bad_cls_nav_time_value:
+        u_inst = u_inst[1:]
+        ui_inst = ui_inst[1:]
+        ncounts_inst = ncounts_inst[1:]
+    ExactTime_ncounts = np.copy(ncounts_inst)
+    ExactTime_ui = np.copy(ui_inst)
+    inst_clk_deltas = delta_datetime_vector(u_inst)
 
     # Estimate the data rate from the Nav time and length of data array.
     # Warn user if computed data doesn't match initialized data rate.
@@ -573,66 +574,69 @@ elif Nav_source == 'cls':
         print(attention_bar)
         print("!!!!!!! WARNING !!!!!!!")
         print("The estimated (init. file) and computed data rates differ by too much.")
-        print("estimated: ",CLS_hz," computed: ",computed_CLS_hz)
+        print("estimated: ", CLS_hz, " computed: ", computed_CLS_hz)
         print("Make sure CLS_hz from initializations is correct")
         print("!!!!!!! WARNING !!!!!!!")
-        print('Enter "c" to contine processing anyway.')
+        print('Enter "c" to continue processing anyway.')
         print('Enter "q" to safely quit.')
         print('If you continue, data rate from initializations will be used.')
         print(attention_bar)
         pdb.set_trace()
     else:
         print('Init. file CLS data rate is sufficiently close to computed data rate.')
-        print('CLS_hz: ',CLS_hz, ' computed_CLS_hz: ',computed_CLS_hz)
+        print('CLS_hz: ', CLS_hz, ' computed_CLS_hz: ', computed_CLS_hz)
         print('To be clear, code will use CLS data rate from initializations (CLS_hz)\n')
     m = 1.0 / CLS_hz
 
-    # Create x-ord array of Unix Times at which to compute interpolated values
-    ix = np.arange(Nav_UnixT[0],Nav_UnixT[0]+tot_elapsed+1,m) # x coordinates of the interpolated values
-
+    # interp_start_time_offset calculation moved up on 5/4/2020 for availability in ix generation
     # Create the x-coordinates for the interp data, which will have units of Unix Time
     print("Making the reasonably dangerous assumption that earliest time is first rec")
     print("and latest time is last rec.")
     rough_expected_recs_sec = int(CLS_hz)
     interp_start_time_offset = 0.0
-    if ncounts[0] < CLS_hz:
-        interp_start_time_offset = (rough_expected_recs_sec - ncounts[0])/CLS_hz
-        
+    # if ncounts[0] < CLS_hz:      # 5/5/2020 Removed for instances that ncounts is larger than expected 1 second
+    interp_start_time_offset = (rough_expected_recs_sec - ncounts[0]) / CLS_hz  # Now calculated using the ncounts from NAV DATA
+
+    # Added section below so that we can correctly determine the end of time in the data
+    if ncounts[-1] < CLS_hz:     # Now calculated using the ncounts from NAV DATA
+        interp_end_time_offset = (ncounts[-1])/CLS_hz
+
+    # 5/4/2020 ix array calculation updated to include start_time_offset and end_time_offset
+    # and updated addition of 8*m instead of 1
+    # Create x-ord array of Unix Times at which to compute interpolated values
+    print('Try ', (cls_meta_data_all.shape[0] / (((Nav_UnixT[-1] + interp_end_time_offset) - (Nav_UnixT[0] + \
+        interp_start_time_offset) + 1))) * float(nshots), ' for CLS_hz')
+    ix = np.arange((Nav_UnixT[0] + interp_start_time_offset), ((Nav_UnixT[-1] + interp_end_time_offset) + 10*m),
+                   m)  # x coordinates of the interpolated values
+    # ix = np.arange(Nav_UnixT[0], Nav_UnixT[0]+tot_elapsed+1, m)
+
     n_interp = ix.shape[0]
-    nav_interp = np.zeros(n_interp,dtype=CLS_decoded_nav_struct)
+    nav_interp = np.zeros(n_interp, dtype=CLS_decoded_nav_struct)
     Nav_interp_T_float64 = ix
     for field in cls_nav_data_all.dtype.names:
-        if (field == 'UTC_Time'): continue
+        if field == 'UTC_Time': continue
         nav_interp[field] = np.interp(ix, Nav_UnixT, Nav_unique[field])
     # Now handle population of interp time field
-    for k in range(0,n_interp):
-        nav_interp['UTC_Time'][k] = ( cls_nav_data_all['UTC_Time'][0] + DT.timedelta(seconds=(m*k)) )
+    # 5/4/2020 added timedelta of interp_start_time_offset to nav_interp UTC Time values
+    for k in range(0, n_interp):
+        nav_interp['UTC_Time'][k] = (cls_nav_data_all['UTC_Time'][0] + DT.timedelta(seconds=interp_start_time_offset)
+                                     + DT.timedelta(seconds=(m*k)))
  
-    # The purpose of the code in this next paragraph is create the proper mapping
-    # of the CLS data to the interpolated Nav data, which comes from the CLS data
-    # itself when Nav_source=='cls', and there are gaps in the data.
-    # The UTC from the interpolated Nav data is pulled into a new array, UTC_ovrwrt.
-    # At this point, cls_meta_data_all has exactly the # of total valid (non-watchdog)
-    # records in the entire flight. But before this paragraph was written, it only did the
-    # thing in the else block - and this allows the CLS records to map to the Nav data inside
-    # itself. cls_meta_data_all is thrown into the create_cls_interp_unixt_array function
-    # in order allow the code to match the CLS data to itself (ie the interpolated Nav data). The
-    # ORIG and INTERP Unix Times that go into the C mapping function come from cls_meta_data_all['Nav']
-    # implanted into the record-space of a single CLS file via the map variable, "Navi."
-    # If there are data gaps, the interpolated data (pre-version 3) is interpolated across the gaps,
-    # resulting in a much longer (many more profiles) array than cls_meta_data_all. This means
-    # that interpolated Nav data now residing in cls_meta_data_all will map incorrectly, the error
-    # increasing after each data gap. These are large errors. By deleting the interpolated
-    # gap-spanning data, cls_meta_data_all will implant each CLS file with correctly
-    # mapped interpolated navigation (to match to itself). Simple, right?
-    # Test this code out on SEAC4RS data.
-    offset_indx=np.abs((ix - ix[0]) - interp_start_time_offset).argmin()
+    # Prepare nav_interp 'UTC_Time' array, where time gaps have been
+    # masked out, then overwrite original Nav time.
+    # 5/4/2020 replaced calculation offset_indx = np.abs((ix - ix[0]) - interp_start_time_offset).argmin()
+    offset_indx = (rough_expected_recs_sec - ncounts[0])   # Uses Nav Time for ncounts
     if inst_clk_deltas.max() > inst_clk_rez:
         
+        print('If you are seeing this message, tell Patrick Selmer to')
+        print('investigate why this block of code is necessary.')
+        # Maybe this relates to how NEW is populated in the C code.
+        # Wish I would have take better notes about this block.
+        pdb.set_trace()
         print(attention_bar+"Time gaps in instrument clock detected. Handling it!"+attention_bar)
         UTC_ovrwrt = np.copy(nav_interp['UTC_Time'][offset_indx:])
         no_delta = True
-        for k in range(0,inst_clk_deltas.shape[0]):
+        for k in range(0, inst_clk_deltas.shape[0]):
             if inst_clk_deltas[k] > inst_clk_rez:
                 if no_delta: 
                     delta_indx = ExactTime_ui[k-1]+ExactTime_ncounts[k-1]
@@ -643,7 +647,7 @@ elif Nav_source == 'cls':
                 #print('A')
                 #pdb.set_trace()
                 delta_indx += skip_recs
-            elif ((not no_delta) and (inst_clk_deltas[k] <= inst_clk_rez)):
+            elif (not no_delta) and (inst_clk_deltas[k] <= inst_clk_rez):
                 delta_indx += ExactTime_ncounts[k] #int(CLS_hz)
                 #print(delta_indx,ExactTime_ui[k],k)
                 #print('B')
@@ -662,18 +666,22 @@ elif Nav_source == 'cls':
     else:
         
         print('\nYou are lucky. No time gaps detected in the middle of the flight! :-)\n')
-        cls_meta_data_all['Nav']['UTC_Time'] = nav_interp['UTC_Time'][offset_indx:offset_indx+cls_meta_data_all.shape[0]]
+        # 5/4/2020 offset_indx removed from assignment below and will need to be checked for lines above
+        print(cls_meta_data_all['Nav']['UTC_Time'].shape[0])
+        print(nav_interp['UTC_Time'].shape[0])
+        cls_meta_data_all['Nav']['UTC_Time'] = nav_interp['UTC_Time'][0:cls_meta_data_all.shape[0]]
 
-    #plt.plot_date(cls_meta_data_all['Nav']['UTC_Time'],cls_meta_data_all['Nav']['RollAngle'],marker='x')
-    #plt.plot_date(nav_interp['UTC_Time'],nav_interp['RollAngle'],marker='o')
-    #plt.show()
+    # plt.plot_date(cls_meta_data_all['Nav']['UTC_Time'],cls_meta_data_all['Nav']['RollAngle'],marker='x')
+    # plt.plot_date(nav_interp['UTC_Time'],nav_interp['RollAngle'],marker='o')
+    # plt.show()
+    # pdb.set_trace()
     
     # NOW, set the array that will be used in processing, cls_nav_data_all equal
     # to the interpolated array that was just created, nav_interp.
     cls_nav_data_all = nav_interp
     
     # This variable will be the same no matter what the Nav_source
-    Nav_hz = CLS_hz    
+    Nav_hz = CLS_hz      
     
 else:
     
@@ -1294,7 +1302,7 @@ for f in range(0,nCLS_files):
             print("I guess the time_bins lined up perfectly with edge of previous file")
             print("because there are no values in the previous file's last time bin.")
             print(trans_bin)
-            print(attention_bar)
+            print(attention_bar)         
 
         for tb in range(si,ei):
             for field in Nav_save_avg.dtype.names:
@@ -1474,13 +1482,16 @@ print('Main L1A execution finished at: ',DT.datetime.now())
 print('Shape of rectrack_master: ',rectrack_master.shape)
 print('Shape of L1Arec_nums: ',L1Arec_nums.shape)
 print('Shape of L1A_Time: ',L1A_Time.shape)
+testdt = []
 index_map_file = L1_dir + 'CLS2L1A_map_'+proj_name+'_'+flt_date+'_'+Nav_source+'.csv'
 with open(index_map_file, 'w') as map_f_obj:
     for CLSi, L1Ai in zip(rectrack_master, L1Arec_nums):
         str_time = str( L1A_Time[L1Ai,:].view('S26') )[3:29]
+        testdt.append(DT.datetime.strptime(str_time,"%Y-%m-%dT%H:%M:%S.%f"))
         string2write = str(CLSi)+','+str(L1Ai)+','+str_time+'\n'
         map_f_obj.write(string2write)
-
+#testdt = np.asarray(testdt)
+#pdb.set_trace()
 # Write out any final parameters to the HDF5 file that needed to wait
 
 num_recs_dset[:] = nrecs

L1A/make_cls2l1a_map_cpl_l1a_v3.py
@@ -301,7 +301,6 @@
 # and file boundary line up, the array's get expanded to accomodate the
 # transfer bin. So it's now possible for a transfer bin to contain a full
 # secs2avg worth of raw profiles.
-# ALSO, posgap_Nav now used in CPL Nav pre-processing block.
 
 # Import libraries <----------------------------------------------------
 
@@ -886,7 +885,7 @@ elif Nav_source == 'cls':
             posgap_Nav = cls_nav_data_all[good_time_indexes][mti]
             for i in range(1, index_diffs[mti]):
                 ii = i + good_time_indexes[mti - 1]
-                cls_nav_data_all[ii] = posgap_Nav
+                cls_nav_data_all[ii] = pregap_Nav
         # Once the bad times are seen again in the individual-file CLS data in
         # the file loop ('core processing'), then the process_start initializations
         # variable should take care of things.
@@ -1347,6 +1346,10 @@ trans_bin = [0, 0]
 trans_total = 0
 last_file = False
 OL_map_indx = 0  # count through overlap map as required
+######## CLS 2 L1A MAP-SAVING CODE ########
+true_undoctored_num_CLS_recs = 0      # not a typical L1A variable
+last_true_undoctored_num_CLS_recs = 0 # not a typical L1A variable
+n_L1A = 0 # counts the number of L1A-resolution records
 for f in range(0, nCLS_files):
 
     if f not in usable_file_indicies:
@@ -1375,6 +1378,23 @@ for f in range(0, nCLS_files):
     nr_1file = CLS_data_1file.shape[0]  # # of records in current file
     good_rec_bool = np.ones(CLS_data_1file.shape[0], dtype=bool)  # is 'True' for good records, 'False' for bad
     satur_flag = False  # Will turn True if any bin in current file is potentially saturated.
+    ################## MASTER CLS RECORD TRACKER FOR SINGLE FILE ###############################
+    if FL_flag == -1:  # first file of flight
+        true_undoctored_num_CLS_recs = nr_1file + FL_trunc[0] + last_true_undoctored_num_CLS_recs
+        rectrack_1file = np.arange(last_true_undoctored_num_CLS_recs, true_undoctored_num_CLS_recs, dtype=np.uint32)
+        rectrack_1file = rectrack_1file[FL_trunc[0]:] 
+    elif FL_flag == 1: # last file of flight
+        true_undoctored_num_CLS_recs = nr_1file + FL_trunc[1] + last_true_undoctored_num_CLS_recs
+        rectrack_1file = np.arange(last_true_undoctored_num_CLS_recs, true_undoctored_num_CLS_recs, dtype=np.uint32)
+        rectrack_1file = rectrack_1file[:int(-1*FL_trunc[1])]
+    else:              # flight in the middle
+        true_undoctored_num_CLS_recs = nr_1file + last_true_undoctored_num_CLS_recs
+        rectrack_1file = np.arange(last_true_undoctored_num_CLS_recs, true_undoctored_num_CLS_recs, dtype=np.uint32)
+    #print(true_undoctored_num_CLS_recs,last_true_undoctored_num_CLS_recs)
+    #print(rectrack_master.min(), rectrack_master.max(), rectrack_master.shape)
+    #pdb.set_trace()
+    last_true_undoctored_num_CLS_recs = true_undoctored_num_CLS_recs
+    ############################################################################################    
 
     # Create histogram bin boundaries. If first_read, created farther down in first_read block.
     if not first_read: time_bins = np.arange(trans_bin[0], CLS_UnixT_float64_orig[-1] + 3.0 * secs2avg, secs2avg)
@@ -1386,6 +1406,7 @@ for f in range(0, nCLS_files):
         time_range_mask_high = CLS_data_1file['meta']['Header']['ExactTime'] < process_end
         time_range_mask = time_range_mask_low * time_range_mask_high
         CLS_data_1file = np.extract(time_range_mask, CLS_data_1file)
+        rectrack_1file = np.extract(time_range_mask,rectrack_1file)
         CLS_UnixT_float64_orig = np.extract(time_range_mask, CLS_UnixT_float64_orig)
         good_rec_bool = np.ones(CLS_data_1file.shape[0], dtype=bool)  # is 'True' for good records, 'False' for bad
         print(attention_bar)
@@ -1837,6 +1858,7 @@ for f in range(0, nCLS_files):
     tot_good_recs = good_rec_bool.sum()
     if tot_good_recs < nr_1file:
         CLS_UnixT_float64_new = CLS_UnixT_float64_new[good_rec_bool]
+        rectrack_1file = rectrack_1file[good_rec_bool]
         Nav_save = Nav_save[good_rec_bool]
         laserspot = laserspot[good_rec_bool, :]
         ONA_save = ONA_save[good_rec_bool]
@@ -1870,6 +1892,9 @@ for f in range(0, nCLS_files):
     # computed differently for the edge versus middle files. The reason averaging
     # is done one file at a time is to limit usage of RAM on the computer.
     if secs2avg > 0.0:  # if < 0, don't average
+        
+        ######## CLS 2 L1A MAP-SAVING CODE ########
+        L1Arec_nums_1file = np.empty_like(rectrack_1file)
 
         bin_numbers = np.digitize(CLS_UnixT_float64_new, time_bins)
         u, ui, ncounts = np.unique(bin_numbers, return_index=True, return_counts=True)
@@ -1916,6 +1941,14 @@ for f in range(0, nCLS_files):
             NRB_avg[:, 0, :] = (NRB_sum + NRB[:, rr:rr + ncounts[0], :].sum(axis=1)) / trans_total
             bg_save_avg[:, 0] = (bg_save_sum + bg_save[:, rr:rr + ncounts[0]].sum(axis=1)) / trans_total
             saturate_ht_max[:, 0] = np.asarray((saturate_ht_carryover.max(axis=1),saturate_ht[:, rr:rr + ncounts[0]].max(axis=1))).max(axis=0)
+            ######## CLS 2 L1A MAP-SAVING CODE ########
+            # This block of code is where the transfer bin is carried over,
+            # so set the current L1A record number equal to the last value
+            # from the previous file iteration. In the transfer save block,
+            # n_L1A is not incremented, so the following line is good.
+            L1Arec_nums_1file[rr:rr+ncounts[0]] = n_L1A 
+            # Now increment n_L1A because you're done with the transfer record
+            n_L1A += 1
             print('trans_ncounts = ', trans_ncounts)
             rr += ncounts[0]
         else:
@@ -1957,7 +1990,15 @@ for f in range(0, nCLS_files):
             # Now do these critical arrays as well
             u = np.concatenate(([-99],u))        # now elem 0 corr. with trans_bin
             ui = np.concatenate(([-99],ui))
-            ncounts = np.concatenate(([trans_ncounts],ncounts))          
+            ncounts = np.concatenate(([trans_ncounts],ncounts))
+            ######## CLS 2 L1A MAP-SAVING CODE ########
+            # This block of code is where the transfer bin is carried over,
+            # so set the current L1A record number equal to the last value
+            # from the previous file iteration. In the transfer save block,
+            # n_L1A is not incremented, so the following line is good.
+            L1Arec_nums_1file[rr:rr+ncounts[0]] = n_L1A 
+            # Now increment n_L1A because you're done with the transfer record
+            n_L1A += 1            
             print('trans_ncounts = ', trans_ncounts)                       
             si = 1
             ei = Nav_save_avg.shape[0] - 1 # You expanded the array in this block
@@ -1984,6 +2025,9 @@ for f in range(0, nCLS_files):
             NRB_avg[:, tb, :] = np.mean(NRB[:, rr:rr + ncounts[tb], :], axis=1)
             bg_save_avg[:, tb] = np.mean(bg_save[:, rr:rr + ncounts[tb]], axis=1)
             saturate_ht_max[:, tb] = np.max(saturate_ht[:, rr:rr + ncounts[tb]], axis=1)
+            ######## CLS 2 L1A MAP-SAVING CODE ########
+            L1Arec_nums_1file[rr:rr+ncounts[tb]] = n_L1A
+            n_L1A += 1            
             # print('iter: ',u[tb], CLS_UnixT_float64_new[rr])
             rr += ncounts[tb]
 
@@ -2005,6 +2049,8 @@ for f in range(0, nCLS_files):
             NRB_sum = NRB[:, rr:rr + ncounts[-1], :].sum(axis=1)
             bg_save_sum = bg_save[:, rr:rr + ncounts[-1]].sum(axis=1)
             saturate_ht_carryover = saturate_ht[:, rr:rr + ncounts[-1]]
+            ######## CLS 2 L1A MAP-SAVING CODE ########
+            L1Arec_nums_1file[rr:rr+ncounts[-1]] = n_L1A            
             # following line defines the "transfer" bin; trans_bin
             trans_bin = time_bins[bin_numbers[ui[-1]] - 1: bin_numbers[ui[-1]] + 1]
             trans_ncounts = ncounts[-1]
@@ -2030,6 +2076,17 @@ for f in range(0, nCLS_files):
             ncounts = ncounts[big_enough_mask]
             ui = ui[big_enough_mask]
             u = u[big_enough_mask]
+            ######## CLS 2 L1A MAP-SAVING CODE ########
+            # 1) Identify the L1A rec #'s that have been deleted.
+            # 2) Find where the L1Arec_nums_1file array == those rec #'s
+            # 3) Remove those elements from L1Arec_nums_1file & rectrack_master_1file
+            unique_L1Arec_nums = np.unique(L1Arec_nums_1file)
+            too_small_mask = np.invert(big_enough_mask)
+            deleted_L1A_indicies = unique_L1Arec_nums[too_small_mask]
+            for di in deleted_L1A_indicies:
+                mask = L1Arec_nums_1file != di # locations not equal to deleted index
+                L1Arec_nums_1file = L1Arec_nums_1file[mask] # running count of # of L1A records
+                rectrack_1file = rectrack_1file[mask]
             print("\nAvg'd profiles eliminated due to min_avg_profs constraint.")
             print(np.argwhere(big_enough_mask == False))
             print(big_enough_mask.shape, " reduced to ", u.shape, "\n")
@@ -2069,6 +2126,10 @@ for f in range(0, nCLS_files):
         bg_dset[:, expanded_length - n_expand:expanded_length] = bg_save_avg[:, cutbegin:n_expand + cutbegin]
         saturate_ht_dset[:, expanded_length - n_expand:expanded_length] = saturate_ht_max[:,cutbegin:n_expand + cutbegin]
         nrecs = expanded_length
+        print('Nav_save_avg shape: ',Nav_save_avg.shape)
+        print('cutbegin: ',cutbegin)
+        print('n_expand: ',n_expand)
+        print('n_L1A: ',n_L1A)
 
     else:  # No averaging
 
@@ -2098,6 +2159,19 @@ for f in range(0, nCLS_files):
         bg_dset[:, i - nr_1file:i] = bg_save
         saturate_ht_dset[:, i - nr_1file:i] = saturate_ht
         nrecs = i
+        print('** WARNING ** WARNING ** WARNING ** WARNING ** WARNING **')
+        print('No code written to deal with no averaging')   
+        
+    # At this point, no more raw CLS records will be deleted. So now, concatenate
+    # rectrack_master with rectrack_1file.
+    if 'rectrack_master' in locals(): # Will not be in locals() first iter.
+        rectrack_master = np.concatenate((rectrack_master,rectrack_1file))
+        L1Arec_nums = np.concatenate((L1Arec_nums, L1Arec_nums_1file))
+        L1A_Time = np.concatenate((L1A_Time, Nav_save_avg['UTC'][cutbegin:n_expand+cutbegin]), axis=0)
+    else:
+        rectrack_master = np.copy(rectrack_1file)
+        L1Arec_nums = np.copy(L1Arec_nums_1file)
+        L1A_Time = np.copy(Nav_save_avg['UTC'][cutbegin:n_expand+cutbegin])             
 
     first_read = False
 
@@ -2107,8 +2181,23 @@ for f in range(0, nCLS_files):
 
 print('Main L1A execution finished at: ', DT.datetime.now())
 
-# Write out any final parameters to the HDF5 file that needed to wait
+######## CLS 2 L1A MAP-SAVING CODE ########
+# Now write out the CLS-to-L1A index maps to a text file
+print('Shape of rectrack_master: ',rectrack_master.shape)
+print('Shape of L1Arec_nums: ',L1Arec_nums.shape)
+print('Shape of L1A_Time: ',L1A_Time.shape)
+testdt = []
+index_map_file = L1_dir + 'CLS2L1A_map_'+proj_name+'_'+flt_date+'_'+Nav_source+'.csv'
+with open(index_map_file, 'w') as map_f_obj:
+    for CLSi, L1Ai in zip(rectrack_master, L1Arec_nums):
+        str_time = str( L1A_Time[L1Ai,:].view('S26') )[3:29]
+        testdt.append(DT.datetime.strptime(str_time,"%Y-%m-%dT%H:%M:%S.%f"))
+        string2write = str(CLSi)+','+str(L1Ai)+','+str_time+'\n'
+        map_f_obj.write(string2write)
 
+# Write out any final parameters to the HDF5 file that needed to wait
+#testdt = np.asarray(testdt)
+#pdb.set_trace()
 num_recs_dset[:] = nrecs
 hdf5_file.close()
 GEOS_fobj.close()